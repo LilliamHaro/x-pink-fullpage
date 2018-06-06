ca342d4774b6a78a918fa7743f58e9964893877c
LilliamHaro <qualia950@gmail.com>
2018-06-06 00:03:16 -0500

    usando rutas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     // inicializando el plugin fullpage.js
-    $('#wrapper').fullpage();
+    $('#wrapper').fullpage({
+        //opcion si para las no se quieres el atributo data-anchor en cada section
+        // anchors:['firstPage', 'secondPage', 'thirdPage']
+    });
     
 });
